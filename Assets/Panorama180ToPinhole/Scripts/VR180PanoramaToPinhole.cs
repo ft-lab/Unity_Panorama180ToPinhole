b1e158ce2a03f67609f4a300454f42bbf261136e
ft-lab (Yutaka Yoshisaka) <y_yutaka@fa2.so-net.ne.jp>
2023-09-17 16:11:39 +0900

    Update.

--- Assets/Panorama180ToPinhole/Scripts/VR180PanoramaToPinhole.cs
+++ Assets/Panorama180ToPinhole/Scripts/VR180PanoramaToPinhole.cs
@@ -6,11 +6,13 @@ using UnityEngine.Video;
 using System;
 using System.IO;
 
-namespace PanoramaToPinhole
+namespace Panorama180ToPinhole
 {
     [RequireComponent(typeof(Camera))]
     public class VR180PanoramaToPinhole : MonoBehaviour
     {
+        [SerializeField] VideoClip VR180VideoClip;   // VR180のmp4を指定.
+
         [SerializeField] float CameraFOV = 60.0f;   // 視野角度.
         [SerializeField] float CameraTilt = 40.0f;   // 各カメラの傾き.
         [SerializeField] Vector2Int TextureSize = new Vector2Int(800, 600);   // テクスチャサイズ.
@@ -20,6 +22,8 @@ namespace PanoramaToPinhole
         private List<GameObject> m_camerasList = null;              // Pinhole投影を行うカメラ.
         private List<RenderTexture> m_renderTextureList = null;     // RenderTexture.
 
+        private GameObject m_HalfSphere = null;     // 半球型の動画を貼り付けるGameObject.
+
         private GameObject m_videoG;        // VideoPlayerのGameObject.
         private VideoPlayer m_videoPlayer;  // Video Player.
 
@@ -28,24 +32,75 @@ namespace PanoramaToPinhole
         private bool m_outputBusy = false;  // Coroutine実行中の場合はtrue.
 
         private Texture2D m_tex = null;     // 作業用のテクスチャ.
-        private bool m_finished = false;    // 完了したらtrue.
 
         // Start is called before the first frame update
         void Start()
         {
+            // メインカメラのパラメータを変更.
+            UpdateMainCameraParameters();
+
+            // 半球を作成.
+            LoadHalfSphere();
+
             // カメラを作成.
             CreatePinholeCameras();
+
+            // Video Playerを作成.
+            CreateVideoPlayer();
 
             // Videoの初期化.
             InitVideo();
 
+            // Videoを再生.
             if (m_videoPlayer != null) {
                 m_videoPlayer.Play();
             }
+
             m_outputBusy = false;
             m_curTime = 0.0;
             m_counter = 0;
-            m_finished = false;
+        }
+
+        void OnDestroy()
+        {
+            if (m_HalfSphere != null) Destroy(m_HalfSphere);
+            if (m_tex != null) Destroy(m_tex);
+            if (m_videoG != null) Destroy(m_videoG);
+            for (int i = 0; i < m_renderTextureList.Count; ++i) {
+                if (m_renderTextureList[i] != null) Destroy(m_renderTextureList[i]);
+            }
+            m_HalfSphere = null;
+            m_tex = null;
+            m_renderTextureList = null;
+            m_videoG = null;
+        }
+
+        /**
+         * メインカメラのパラメータを変更.
+         */
+        void UpdateMainCameraParameters()
+        {
+            Camera c = gameObject.GetComponent<Camera>();
+            if (c != null) {
+                c.farClipPlane = Math.Max(c.farClipPlane, 100000.0f);
+                gameObject.transform.localPosition = Vector3.zero;
+                gameObject.transform.localRotation = Quaternion.identity;
+                gameObject.transform.localScale    = Vector3.one;
+            }
+        }
+
+        /**
+         * 半球のオブジェクトをロード.
+         */
+        void LoadHalfSphere(float scaleV = 8000.0f)
+        {
+            if (m_HalfSphere != null) return;
+
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/HalfSphere");
+            if (prefab != null) {
+                m_HalfSphere = Instantiate(prefab, Vector3.zero, Quaternion.Euler(0, 180, 0));
+                m_HalfSphere.transform.localScale = new Vector3(scaleV, scaleV, scaleV);
+            }
         }
 
         // Update is called once per frame
@@ -99,12 +154,14 @@ namespace PanoramaToPinhole
             {
                 double delta = 1.0 / Math.Max(CaptureFPS, 0.0001);
                 if (m_curTime + delta >= m_videoPlayer.length) {
-                    m_finished = true;
                     Debug.Log("Finished!");
                 }
             }
         }
 
+        /**
+         * RenderTextureを画像ファイルとして保存.
+         */
         void SaveRenderTextureToFile(RenderTexture rt, string fileName)
         {
             if (rt == null) return;
@@ -125,9 +182,9 @@ namespace PanoramaToPinhole
         /**
          * Pinholeのカメラを作成.
          * 以下を生成する.
-         * RenderTexture (Asset).
-         * Material (Asset).
-         * Camera
+         *   RenderTexture (Asset).
+         *   Material (Asset).
+         *   Camera
          */
         void CreatePinholeCameras()
         {
@@ -211,16 +268,36 @@ namespace PanoramaToPinhole
             }
         }
 
+
+        /**
+         * Video Playerを作成.
+         */
+        void CreateVideoPlayer()
+        {
+            m_videoG = new GameObject("VR180_VideoPlayer");
+            m_videoPlayer = m_videoG.AddComponent<VideoPlayer>();
+        }
+
         /**
          * Video Playerの再生を初期化（停止）.
          */
         void InitVideo()
         {
-            m_videoG = GameObject.Find("/Video Player");
-            if (m_videoG == null) return;
+            if (m_videoG == null || m_HalfSphere == null) return;
+
+            MeshRenderer meshR = m_HalfSphere.GetComponent<MeshRenderer>();
+            if (meshR == null) return;
+            Material mat = meshR.sharedMaterial;
 
             m_videoPlayer = m_videoG.GetComponent<VideoPlayer>();
+            m_videoPlayer.clip = VR180VideoClip;
             m_videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+
+            Texture tex = mat.GetTexture("_MainTex");
+            if (tex != null && (tex is RenderTexture)) {
+                m_videoPlayer.targetTexture = (RenderTexture)tex;
+            }
+
             m_videoPlayer.isLooping = false;
             m_videoPlayer.audioOutputMode = VideoAudioOutputMode.None;
 
